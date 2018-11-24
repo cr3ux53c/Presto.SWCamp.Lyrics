a3e9e886bbecec22b579be633057ff38be05ef49
Lim HyunSeok <cr3ux53c@gmail.com>
2018-11-24 14:06:13 +0900

    일단 성공

--- Presto.SWCamp.Lyrics/LyricsWindow.xaml.cs
+++ Presto.SWCamp.Lyrics/LyricsWindow.xaml.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
@@ -20,12 +21,10 @@ namespace Presto.SWCamp.Lyrics {
     public partial class LyricsWindow : Window {
         String[] lyricsRaw;
         DispatcherTimer timer;
-        Dictionary<String, String> lyrics;
         List<String> list;
         List<TimeSpan> time;
-        
+        int currentLyricIndex = 0;
 
-        int timing = 0;
         public LyricsWindow() {
             InitializeComponent();
 
@@ -38,28 +37,48 @@ namespace Presto.SWCamp.Lyrics {
             lyricsRaw = File.ReadAllLines(filePath.Substring(0, filePath.Length-3) + "lrc");
             list = new List<string>();
             time = new List<TimeSpan>();
+
+            //
             foreach (var line in lyricsRaw) {
+                if (line[1] > 64) {
+                    continue;
+                }
                 int threshold = line.IndexOf(']');
                 list.Add(line.Substring(threshold+1));
                 String str = line.Substring(1, threshold-1);
                 var timeSplited = str.Split(new char[] { ':', '.' });
-                int.Parse(timeSplited[0]);
                 time.Add(new TimeSpan(0, 0, int.Parse(timeSplited[0])
                                             , int.Parse(timeSplited[1])
-                                            , int.Parse(timeSplited[2])));
+                                            , int.Parse(timeSplited[2])*10));
+                // 밀리세컨드는 100 곱해야 함.
+
+                Regex regex = new Regex("[^[]]");
+
+                regex.IsMatch("fdsafdsa");
             }
 
-
+            
             // 타이밍
             timer = new DispatcherTimer {
-                Interval = TimeSpan.FromMilliseconds(1000)
+                Interval = TimeSpan.FromMilliseconds(10)
             };
             timer.Tick += Timer_Tick;
             timer.Start();
         }
 
         private void Timer_Tick(object sender, EventArgs e) {
-            text_lyrics.Text = list[timing++];
+            int currentPlayTime = (int)PrestoSDK.PrestoService.Player.Position;
+
+            if (currentPlayTime < time[0].TotalMilliseconds-1000*10) {
+                text_lyrics.Text = PrestoSDK.PrestoService.Player.CurrentMusic.Title + " - " + PrestoSDK.PrestoService.Player.CurrentMusic.Artist.Name;
+            }
+
+            if (currentPlayTime > time[currentLyricIndex].TotalMilliseconds) {
+                text_lyrics.Text = list[currentLyricIndex++];
+            } else {
+                if (currentLyricIndex > 0)
+                    text_lyrics.Text = list[--currentLyricIndex];
+            }
         }
     }
 }
